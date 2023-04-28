c5a741f79ccc7e180910c4c0bc24ced706f39205
john-bush <johncbus@usc.edu>
2023-04-28 02:36:12 -0700

    revised the algorithm, now getting more reasonable results. Still need to verify timestamps and tune parameters further though

--- src/SegmentedVideoFrameClustering.java
+++ src/SegmentedVideoFrameClustering.java
@@ -11,31 +11,34 @@ import javax.imageio.ImageIO;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import org.apache.commons.math4.legacy.linear.Array2DRowRealMatrix;
+import org.apache.commons.math4.legacy.linear.RealMatrix;
 import org.apache.commons.math4.legacy.linear.SingularValueDecomposition;
 
 public class SegmentedVideoFrameClustering {
     private static final DecimalFormat df = new DecimalFormat("00.00");
     private static List<BufferedImage> frames = new ArrayList<>();
 
-    static final int num_block_rows = 3;
-    static final int num_block_cols = 3;
+    static final int num_block_rows = 5;
+    static final int num_block_cols = 5;
     static final int num_histograms = 4;
     static final int num_bins = 256;
     private static final int segment_length = 25; // number of frames in segment
     private static final int frame_step = 4; // frame step used for cut verification
 
 //    THRESHOLDS AND TUNING PARAMETERS
-    static final double epsilon = 0.00001;
+    static final double epsilon = 0.0001;
     static final double TC1 = 0.75;
-    static final double TC2 = 0.9;
-    static final double correlation_threshold = 0.95;
+    static final double TC2 = 0.90;
+    static final double correlation_threshold = 0.96;
+    static final int DEBUG = 0; // to show correlation differences
     public static void main(String[] args) {
-        File file = new File("lib/Ready_Player_One_rgb/InputVideo.rgb"); // name of the RGB video file
+        File file = new File("lib/The_Great_Gatsby_rgb/InputVideo.rgb"); // name of the RGB video file
         int width = 480; // width of the video frames
         int height = 270; // height of the video frames
         int fps = 30; // frames per second of the video
         int numFrames = 8682; // number of frames in the video
 
+        System.out.println("Blocks:"+num_block_rows+"x"+num_block_cols+", Thresholds: "+TC1+", " + TC2 + ", Epsilon: " + epsilon);
 
         try {
             RandomAccessFile raf = new RandomAccessFile(file, "r");
@@ -60,14 +63,21 @@ public class SegmentedVideoFrameClustering {
             }
 
             // calculate segment beginning-end frame correlations
-            for (int i = 0; i < numFrames - segment_length - 1; i+=segment_length - 1) {
+            for (int i = 0; i < numFrames - segment_length - 1; i+=(segment_length - 1)) {
                 // compute correlation between first and last frames of segment
-                double correlation = computeCorrelation(i, i+segment_length - 1);
+                double correlation = computeCorrelation(i, i + segment_length - 1);
 
-                if (correlation < correlation_threshold) { // segment is dynamic
+                if (DEBUG > 2) {
+                    System.out.println("Segment Correlation: " + correlation);
+                }
+
+                if (correlation <= correlation_threshold) { // segment is dynamic
 //      perform cut verification as covered in the paper
-                    int rel_frame = cutTransitionID(i);
+                    int rel_frame = cutTransitionID_Incremental(i);
                     if (rel_frame != -1) {
+                        if (DEBUG == 2) {
+                            System.out.print("Segment Frame: " + i + ", Local Frame: " + rel_frame + ", Shot: ");
+                        }
                         int cut_frame = i + rel_frame;
 
                         // Start a new group
@@ -106,13 +116,16 @@ public class SegmentedVideoFrameClustering {
 //        TODO: these for loop bounds are super sketchy
         // calculate distances on frame step
         double[] distances = new double[segment_length / (frame_step + 1)];
-        System.out.print(", Distances: [");
+        if (DEBUG > 0){
+            System.out.print(", Distances: [");
+        }
         for (int d = 0; d < segment_length / (frame_step + 1); d++) {
             double[] frame1 = matrixBeta[d * frame_step];
             double[] frame2 = matrixBeta[d * frame_step + frame_step];
             distances[d] = computeCorrelation(frame1, frame2);
-
-            System.out.print(distances[d] + ", ");
+            if (DEBUG > 0) {
+                System.out.print(distances[d] + ", ");
+            }
 
             if (distances[d] < TC1) {
                 double min_distance = 100.0;
@@ -126,8 +139,9 @@ public class SegmentedVideoFrameClustering {
                 return cut_index;
             }
         }
-
-        System.out.println("]");
+        if (DEBUG > 0) {
+            System.out.println("]");
+        }
 
         // if all distances > TC1, check for a distance less than TC2
         // if there is only 1 distance < TC2, perform cut localization on the frames within that step.
@@ -135,29 +149,100 @@ public class SegmentedVideoFrameClustering {
         // if more than 1 distance < TC2, do the dynamic verification part of the paper (not written now)
         int threshold_count = 0;
         int threshold_index = -1;
+        double lowest_dist = 5.0;
         for (int i = 0; i < distances.length; i++) {
             if (distances[i] < TC2) {
                 threshold_count++;
-                threshold_index = i;
+
+                if (distances[i] < lowest_dist) {
+                    lowest_dist = distances[i];
+                    threshold_index = i;
+                }
             }
         }
         if (threshold_count == 0) {
             return -1; // segment is static
-        } else if (threshold_count == 1) {
+        } else {
             double min_distance = 100.0;
             for (int i = threshold_index * frame_step; i < (threshold_index * frame_step + frame_step); i++) {
-                double correlation = computeCorrelation(matrixBeta[i], matrixBeta[i+1]);
+                double correlation = computeCorrelation(matrixBeta[i], matrixBeta[i + 1]);
                 if (correlation < min_distance) {
                     min_distance = correlation;
-                    cut_index = threshold_index * frame_step + i + 1;
+                    cut_index = i + 1;
                 }
             }
             return cut_index;
-        } else {
-            return -1;
         }
     }
 
+    private static int cutTransitionID_Incremental(int index) {
+        double[][] matrixBeta = calculateMatrixBeta(index);
+        int cut_index = -1;
+        // look for distance less than TC1
+        // if a distance is less than TC1 is found, calculate the distances between all pairs of frames
+        // within that frame step. The frame pair with a distance less than TC1 is where the cut frame is
+
+//        TODO: these for loop bounds are super sketchy
+        // calculate distances on frame step
+        double[] distances = new double[segment_length - (frame_step + 1)]; // 1 2 3 4 5 6 7 8 9 10
+        if (DEBUG > 0){
+            System.out.print(", Distances: [");
+        }
+        for (int d = 0; d < distances.length; d++) {
+            double[] frame1 = matrixBeta[d];
+            double[] frame2 = matrixBeta[d + frame_step + 1];
+            distances[d] = computeCorrelation(frame1, frame2);
+            if (DEBUG > 0) {
+                System.out.print(distances[d] + ", ");
+            }
+
+            if (distances[d] < TC1) {
+                double min_distance = 100.0;
+                for (int i = d; i < (d + frame_step + 1); i++) {
+                    double correlation = computeCorrelation(matrixBeta[i], matrixBeta[i+1]);
+                    if (correlation < min_distance) {
+                        min_distance = correlation;
+                        cut_index = i + 1;
+                    }
+                }
+                return cut_index;
+            }
+        }
+        if (DEBUG > 0) {
+            System.out.println("]");
+        }
+
+        // if all distances > TC1, check for a distance less than TC2
+        // if there is only 1 distance < TC2, perform cut localization on the frames within that step.
+        // if all distances > TC2, declare the segment static
+        // if more than 1 distance < TC2, do the dynamic verification part of the paper (not written now)
+        int threshold_count = 0;
+        int threshold_index = -1;
+        double lowest_dist = 5.0;
+        for (int i = 0; i < distances.length; i++) {
+            if (distances[i] < TC2) {
+                threshold_count++;
+
+                if (distances[i] < lowest_dist) {
+                    lowest_dist = distances[i];
+                    threshold_index = i;
+                }
+            }
+        }
+        if (threshold_count == 0) {
+            return -1; // segment is static
+        } else {
+            double min_distance = 100.0;
+            for (int i = threshold_index; i < (threshold_index + frame_step + 1); i++) {
+                double correlation = computeCorrelation(matrixBeta[i], matrixBeta[i + 1]);
+                if (correlation < min_distance) {
+                    min_distance = correlation;
+                    cut_index = i + 1;
+                }
+            }
+            return cut_index;
+        }
+    }
 /**
  *       3: Feature Construction (for dynamic segments)
  *           - Construct feature matrix H = [h1, h2, h3, ... h_n], where n is the segment length and
@@ -229,14 +314,19 @@ public class SegmentedVideoFrameClustering {
                 break;
             }
         }
-        System.out.print(", Rank = " + r + ", K = " + k);
+        if (DEBUG > 2) {
+            System.out.print(", Rank = " + r + ", K = " + k);
+        }
         return k;
     }
 
     private static double[][] calculateMatrixBeta(int index) {
         int minutes = ((index / 30) % 3600) / 60;
         double seconds = (index / 30.0) % 60;
-        System.out.print("Processing segment starting at " + minutes + ":" + df.format(seconds) + "s");
+        if (DEBUG > 0) {
+            System.out.print("Processing segment starting at " + minutes + ":" + df.format(seconds) + "s");
+
+        }
 
         // length of each column vector
         final int m = num_block_rows * num_block_cols * num_histograms * num_bins;
@@ -260,18 +350,26 @@ public class SegmentedVideoFrameClustering {
         double[][] truncatedVT = new double[k][svd.getVT().getColumnDimension()];
         svd.getVT().copySubMatrix(0, k - 1, 0, truncatedVT[0].length - 1, truncatedVT);
 
+        double[][] truncatedU = new double[svd.getU().getRowDimension()][k];
+        svd.getU().copySubMatrix(0, truncatedU.length - 1, 0, k - 1, truncatedU);
+
+
 //        NOTE: i am representing this matrix transposed to how its represented in the paper
 //              because it allows us to extract a single B vector as a row of B
-        double[][] Beta_matrix = new double[segment_length][k]; // initialize result vector of length k
+//        double[][] Beta_matrix = new double[segment_length][k]; // initialize result vector of length k
 
+
+        RealMatrix approximatedSvdMatrix = (new Array2DRowRealMatrix(truncatedU)).multiply(new Array2DRowRealMatrix(truncatedS)).multiply(new Array2DRowRealMatrix(truncatedVT)).transpose();
+
+        return approximatedSvdMatrix.getData();
         // calculate the B_matrix
-        for (int b_row = 0; b_row < segment_length; b_row++) {
-            for (int i = 0; i < k; i++) {
-                Beta_matrix[b_row][i] += truncatedS[i][i] * truncatedVT[i][b_row];
-            }
-        }
+//        for (int b_row = 0; b_row < segment_length; b_row++) {
+//            for (int i = 0; i < k; i++) {
+//                Beta_matrix[b_row][i] += truncatedS[i][i] * truncatedVT[i][b_row];
+//            }
+//        }
 
-        return Beta_matrix;
+//        return Beta_matrix;
     }
 
     /**
@@ -336,18 +434,21 @@ public class SegmentedVideoFrameClustering {
                         int green = color.getGreen();
                         int blue = color.getBlue();
 
-                        // luminance equation is from the internet -- supposedly is the perceptual luminance given RGB
-                        int luminance = (int)(0.2126*red) + (int)(0.7152*green) + (int)(0.0722*blue);
-
-                        int red_idx = red + histogram_offset;
-                        int green_idx = green + num_bins + histogram_offset;
-                        int blue_idx = blue + (2 * num_bins) + histogram_offset;
-                        int lum_idx = luminance + (3 * num_bins) + histogram_offset;
+                        int red_idx = (int)(red * (double)(num_bins / 256) + histogram_offset);
+                        int green_idx = (int)(green * (double)(num_bins / 256) + num_bins + histogram_offset);
+                        int blue_idx = (int)(blue * (double)(num_bins / 256)+ (2 * num_bins) + histogram_offset);
 
                         histogram.set(red_idx, histogram.get(red_idx) + 1);
                         histogram.set(green_idx, histogram.get(green_idx) + 1);
                         histogram.set(blue_idx, histogram.get(blue_idx) + 1);
-                        histogram.set(lum_idx, histogram.get(lum_idx) + 1);
+
+                        // luminance equation is from the internet -- supposedly is the perceptual luminance given RGB
+                        if (num_histograms == 4) {
+                            int luminance = (int)(0.2126*red) + (int)(0.7152*green) + (int)(0.0722*blue);
+                            int lum_idx = (int)(luminance * (double)(num_bins / 256) + (3 * num_bins) + histogram_offset);
+
+                            histogram.set(lum_idx, histogram.get(lum_idx) + 1);
+                        }
                     }
                 }
             }
@@ -439,7 +540,7 @@ public class SegmentedVideoFrameClustering {
 
     private static double innerProduct(double[] vector1, double[] vector2) {
         final int m = vector1.length;
-        if (m != vector2.length) {
+        if (m < vector2.length) {
             return 0;
         }
         double sum = 0;
