31a4c3ca30ce6e5a7bf5d37b66aae393d69a0240
Nisha Chatwani <nchatwan@usc.edu>
2023-05-02 15:32:15 -0700

    buttons are hierarchical and number of frames is detected from file length

--- src/VideoPlayer.java
+++ src/VideoPlayer.java
@@ -16,6 +16,8 @@ import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class VideoPlayer {
     public static JFrame frame = new JFrame("Video Display");
@@ -29,13 +31,19 @@ public class VideoPlayer {
     public static int width = 480; // width of the video frames
     public static int height = 270; // height of the video frames
     public static int fps = 30; // frames per second of the video
-    public static int numFrames = 8682; // number of frames in the video
-    // this is pulled from shotTimeCodes.txt
-    public static List<Integer> shotFrames = new ArrayList<Integer>(Arrays.asList(161, 251, 420, 508, 896, 1080, 1131, 1179, 1352, 1844,
-    1958, 2332, 2459, 2583, 2716, 3148, 3244, 3263, 3284, 3303, 3546, 3620, 3729, 3770, 3809, 3848, 3879, 3990, 4023, 4052, 4081, 4129, 4232, 4346, 4492, 4724, 4844, 5329, 5599, 5754, 5952, 6140, 6303, 6857, 6969, 7048, 7458, 7591));
-    public static List<JButton> shotButtons = new ArrayList<>();
+    public static int numFrames = 0; // number of frames in the video
+    public static TreeMap<Integer, List<Integer>> shotFrames = new TreeMap<>();
+    public static List<JButton> hierarchyButtons = new ArrayList<>();
 
     public static void main(String[] args) {
+        // manually adding for testing
+        shotFrames.put(161, Arrays.asList(251, 420, 508));
+        shotFrames.put(896, Arrays.asList(1080, 1131, 1179, 1352, 1844));
+        shotFrames.put(1958, Arrays.asList(2332, 2459, 2583, 2716, 3148, 3244, 3263));
+        shotFrames.put(3284, Arrays.asList(3303, 3546, 3620, 3729, 3770, 3809, 3848, 3879, 3990, 4023, 4052, 4081));
+        shotFrames.put(4129, Arrays.asList(4232, 4346, 4492, 4724, 4844, 5329, 5599, 5754, 5952, 6140, 6303, 6857, 6969, 7048, 7458, 7591));
+        
+        numFrames = (int)(file.length() / (352*288*(fps/8)));
 
         AudioPlayer audioPlayer;
         try {
@@ -46,7 +54,6 @@ public class VideoPlayer {
             System.out.println("Error with audio: " + e.getMessage());
             return;
         }
-
         // Set the layout of the JFrame to BorderLayout
         frame.setSize(new Dimension(width*2, height*2));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,8 +70,9 @@ public class VideoPlayer {
         // Create a JPanel and add it to the JFrame
         frame.add(panel, BorderLayout.SOUTH);
         shotsPanel.setLayout(new BoxLayout(shotsPanel, BoxLayout.Y_AXIS));
-        shotsPanel.setPreferredSize(new Dimension(width-50, height*2));
-        JScrollPane scrollPane = new JScrollPane(shotsPanel);
+        JScrollPane scrollPane = new JScrollPane(shotsPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(width-50, height*2));
         frame.add(scrollPane, BorderLayout.WEST);
 
         // read the video file and save each frame
@@ -125,46 +133,64 @@ public class VideoPlayer {
         pause.addActionListener(pauseListener);
         panel.add(pause);
 
-        // Shot Buttons
-        for(int i = 0; i < shotFrames.size(); i++) {
-            final int final_i = i;
-            shotButtons.add(new JButton("Shot " + (i+1)));
-            shotButtons.get(i).addActionListener(new ActionListener() {
+        // Scene and Shot Buttons
+        int i = 1;
+        for (int sceneFrame : shotFrames.keySet()) {
+            hierarchyButtons.add(new JButton("Scene " + (i)));
+            hierarchyButtons.get(hierarchyButtons.size()-1).addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     playTimer.stop();
-                    currFrame = shotFrames.get(final_i);
-                    System.out.println(shotFrames.get(final_i));
+                    currFrame = sceneFrame;
+                    System.out.println(sceneFrame);
                     playTimer.stop();
                     playTimer.start();
-
                     try {
-                        double c = (double)final_i / (double)shotFrames.size();
+                        double c = (double)sceneFrame / (double)shotFrames.size();
                         long micro_time = (long)(c * (audioPlayer.clip.getMicrosecondLength()));
                         audioPlayer.jump(micro_time);
                     } catch (UnsupportedAudioFileException | IOException | LineUnavailableException err) {
                         System.out.println("Error with Audio: " + err.getMessage());
                     }
-
-
                 }
             });
-            shotsPanel.add(shotButtons.get(i));
+            shotsPanel.add(hierarchyButtons.get(hierarchyButtons.size()-1));
+            i++;
+            int j = 1;
+            for(int shotFrame : shotFrames.get(sceneFrame)) {
+                hierarchyButtons.add(new JButton("Shot " + (j)));
+                hierarchyButtons.get(hierarchyButtons.size()-1).addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        playTimer.stop();
+                        currFrame = shotFrame;
+                        System.out.println(shotFrame);
+                        playTimer.stop();
+                        playTimer.start();
+                        try {
+                            double c = (double)shotFrame / (double)shotFrames.size();
+                            long micro_time = (long)(c * (audioPlayer.clip.getMicrosecondLength()));
+                            audioPlayer.jump(micro_time);
+                        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException err) {
+                            System.out.println("Error with Audio: " + err.getMessage());
+                        }
+                    }
+                });
+                shotsPanel.add(hierarchyButtons.get(hierarchyButtons.size()-1));
+                j++;
+            }
         }
         panel.setBackground(Color.WHITE);
         frame.setSize(width*2, height*2);
         frame.setVisible(true);
-
     }
 
     public static void play() {
-        //System.out.println(currFrame);
         label.setIcon(new ImageIcon(frames.get(currFrame)));
         internalFrame.revalidate();
         internalFrame.repaint();
         frame.validate();
         frame.repaint();
         currFrame++;
-
     }
 }
